449af5724b9c889071c0804062f9514899224afb
Acamerus <danielfurmanov@hotmail.com>
2025-04-06 06:02:12 -0400

    Added files and created Enums Rank/Suit

---
+++ Enums.cs
@@ -0,0 +1,26 @@
+namespace Elevens;
+
+public enum Rank
+{
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+}
+
+public enum Suit
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
